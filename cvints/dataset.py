c3f5e36bdb17fa276008e2cbc68f0049acbdce17
Boychuk Vasily <vboychuk@t-systems.ru>
2020-04-08 16:02:42 +0300

    Update docstring for the Dataset-related classes

--- cvints/dataset.py
+++ cvints/dataset.py
@@ -13,12 +13,30 @@ GREEN = (0, 255, 0)
 
 
 class Dataset:
-    def __init__(self, path_to_data, path_to_annotations_file, is_sampled=False, samples_number=5):
+    """
+        Base wrapper of data for CV tasks
+
+        Attributes
+        ----------
+        path_to_data : str
+            Absolute path to the folder with images and masks
+        path_to_annotations_file : str
+            Absolute path to the annotations file (file should be in COCO format)
+        is_sampled: bool
+            If the value of the parameter is True, to the Dataset will be loaded only
+            `samples_number` images from the `path_to_data` with associated annotations from `path_to_annotations_file`
+            If the value of the parameter is False, all images from `path_to_data` will be loaded to the Dataset
+        samples_number: int
+            The number of images to load to the Dataset if `is_sampled` is True
+    """
+
+    def __init__(self, path_to_data, path_to_annotations_file, is_sampled=False, samples_number=5,
+                 model_evaluation_results=None):
         self.path_to_data = path_to_data
         self.path_to_annotations_file = path_to_annotations_file
         self.is_sampled = is_sampled
         self.path_to_evaluations_results_file = None
-        self.model_evaluation_results = None
+        self.model_evaluation_results = model_evaluation_results
         if self.path_to_annotations_file:
             with open(self.path_to_annotations_file, 'r') as f:
                 annotations_data = json.load(f)
@@ -35,7 +53,51 @@ class Dataset:
             annotations_data = annotations_data['annotations']
             self.annotations = {'images_info': images_info, 'annotations_info': annotations_data}
 
-    def draw_gt_bboxes(self, separately=False, bbox_line_width=3):
+    def set_model_evaluation_results(self, path_to_file_with_evaluation_results):
+        """ Set model evaluation results on this dataset """
+        self.path_to_evaluations_results_file = path_to_file_with_evaluation_results
+        if self.path_to_evaluations_results_file:
+            with open(self.path_to_evaluations_results_file, 'r') as f:
+                self.model_evaluation_results = json.load(f)
+
+
+class ObjectDetectionDataset(Dataset):
+    """
+        Wrapper of data for object detection tasks
+
+        Attributes
+        ----------
+        path_to_data : str
+            Absolute path to the folder with images and masks
+        path_to_annotations_file : str
+            Absolute path to the annotations file (file should be in COCO format)
+        is_sampled: bool
+            If the value of the parameter is True, to the Dataset will be loaded only
+            `samples_number` images from the `path_to_data` with associated annotations from `path_to_annotations_file`
+            If the value of the parameter is False, all images from `path_to_data` will be loaded to the Dataset
+        samples_number: int
+            The number of images to load to the Dataset if `is_sampled` is True
+    """
+
+    def __init__(self, path_to_data, path_to_annotations_file, is_sampled=False, samples_number=5):
+        super(ObjectDetectionDataset, self).__init__(path_to_data, path_to_annotations_file, is_sampled,
+                                                    samples_number)
+
+    def draw_gt_bboxes(self, draw_bboxes_separately=False, bbox_line_width=3):
+        """ Draw ground truth bounding boxes
+
+            Parameters
+            ----------
+            draw_bboxes_separately : bool
+                if True, each bounding box will be drawn sequentially on the new canvas with the picture
+                if False, all bounding box will be drawn simultaneously on the one canvas with the picture
+            bbox_line_width : int
+                the width of the bboxes line
+
+            Returns
+            ----------
+            None
+        """
         shown = False
         images_info = self.annotations['images_info']
         annotation_info = self.annotations['annotations_info']
@@ -50,7 +112,7 @@ class Dataset:
                               (each_gt['bbox'][0] + each_gt['bbox'][2],
                                each_gt['bbox'][1] + each_gt['bbox'][3]),
                               GREEN, bbox_line_width)
-                if separately:
+                if draw_bboxes_separately:
                     plt.imshow(img)
                     plt.show()
                     shown = True
@@ -58,19 +120,41 @@ class Dataset:
                 plt.imshow(img)
                 plt.show()
 
-    def set_model_evaluation_results(self, path_to_file_with_evaluation_results):
-        self.path_to_evaluations_results_file = path_to_file_with_evaluation_results
-        if self.path_to_evaluations_results_file:
-            with open(self.path_to_evaluations_results_file, 'r') as f:
-                self.model_evaluation_results = json.load(f)
 
+class HumanDetectionDataset(ObjectDetectionDataset):
+    """
+        Wrapper of data for the human detection task
 
-class PersonDetectionDataset(Dataset):
+        Attributes
+        ----------
+        path_to_data : str
+            Absolute path to the folder with images and masks
+        path_to_annotations_file : str
+            Absolute path to the annotations file (file should be in COCO format)
+        is_sampled: bool
+            If the value of the parameter is True, to the Dataset will be loaded only
+            `samples_number` images from the `path_to_data` with associated annotations from `path_to_annotations_file`
+            If the value of the parameter is False, all images from `path_to_data` will be loaded to the Dataset
+        samples_number: int
+            The number of images to load to the Dataset if `is_sampled` is True
+    """
     def __init__(self, path_to_data, path_to_annotations_file, is_sampled=False, samples_number=5):
-        super(PersonDetectionDataset, self).__init__(path_to_data, path_to_annotations_file, is_sampled,
-                                                     samples_number)
+        super(HumanDetectionDataset, self).__init__(path_to_data, path_to_annotations_file, is_sampled,
+                                                    samples_number)
 
     def describe_gt(self, with_plots=False):
+        """ Describe data in dataset
+
+            Parameters
+            ----------
+            with_plots : bool
+                if True, the method will print some statistics as text and draw some plots
+                if False, the method will only print some statistics of the data as text
+            
+            Returns
+            ----------
+            None
+        """
         images_number = len(self.annotations['images_info'])
         total_person_number = len(self.annotations['annotations_info'])
         persons_per_image_dict = {}
