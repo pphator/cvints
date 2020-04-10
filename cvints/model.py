2e381e9086090190265d3e0ab51fb274d9553ba4
Boychuk Vasily <vboychuk@t-systems.ru>
2020-04-10 14:37:23 +0300

    update evaluate method of Model class

--- cvints/model.py
+++ cvints/model.py
@@ -4,6 +4,9 @@ import numpy as np
 import json
 
 
+IOU_THRESHOLD_DEFAULT = 0.5
+
+
 class Model:
     """ Base class for models which results we want to explore with this lib
 
@@ -16,7 +19,7 @@ class Model:
         self.test_dataset = test_dataset
 
         self.path_to_detection_results_file = path_to_detection_results_file
-        self.model_evaluation_results = None
+        self.detection_results = None
 
     def evaluate(self):
         pass
@@ -37,10 +40,9 @@ class ObjectDetectionModel(Model):
             with open(self.path_to_detection_results_file, 'r') as f:
                 detection_results = json.load(f)
         # select images from loaded part of test dataset
-
         loaded_test_images_ids = self.test_dataset.get_images_ids()
-        self.model_evaluation_results = [x for x in loaded_test_images_ids if x['image_id'] in loaded_test_images_ids]
-        
+        self.detection_results = [x for x in detection_results if x['image_id'] in loaded_test_images_ids]
+
     @staticmethod
     def get_iou(pred_box, gt_box):
         ixmin = max(pred_box[0], gt_box[0])
@@ -61,9 +63,54 @@ class ObjectDetectionModel(Model):
 
         return iou
 
-    def evaluate(self):
+    def evaluate(self, iou_threshold=IOU_THRESHOLD_DEFAULT):
+        """ Compute metrics for detection results
 
+            Parameters
+            ----------
+            iou_threshold : float
+                The value to say that prediction is correct or incorrect
 
+            Returns
+            ----------
+            precision : float
+            recall : float
+            miss_rate : float
+            results : dict
+        """
+        true_positives = 0
+        number_of_gt_bboxes = 0
+        number_of_predicted_bboxes = 0
+        results = {}
+        for each_prediction in self.detection_results:
+            pred_image_id = each_prediction['image_id']
+            gt_bboxes_of_this_image = [item['bbox'] for item in self.test_dataset.annotations['annotations_info']
+                                       if item['image_id'] == pred_image_id]
+            detection_results = each_prediction['detection_results']
+            predicted_bboxes_of_this_image = [x[0] for x in detection_results]
+            number_of_gt_bboxes_of_this_image = len(gt_bboxes_of_this_image)
+            number_of_predicted_bboxes_of_this_image = len(predicted_bboxes_of_this_image)
 
-        pass
+            # calculate IoU for all combinations of predicted and ground-truth bboxes
+            this_prediction_iou_list = []
+            for each_predicted_bbox in predicted_bboxes_of_this_image:
+                this_predicted_bbox_iou_list = []
+                for each_gt_bbox in gt_bboxes_of_this_image:
+                    this_predicted_bbox_iou_list.append(ObjectDetectionModel.get_iou(each_predicted_bbox, each_gt_bbox))
+                max_iou_for_prediction = np.max(this_predicted_bbox_iou_list)
+                this_prediction_iou_list.append(max_iou_for_prediction)
+
+            # select IoU >= iou_threshold
+            true_positives_this_prediction = len([x for x in this_prediction_iou_list if x >= iou_threshold])
+            false_positives_this_prediction = len([x for x in this_prediction_iou_list if x < iou_threshold])
+            number_of_gt_bboxes += number_of_gt_bboxes_of_this_image
+            number_of_predicted_bboxes += number_of_predicted_bboxes_of_this_image
+            true_positives += true_positives_this_prediction
+            results[pred_image_id] = (true_positives_this_prediction, false_positives_this_prediction, np.mean(this_prediction_iou_list))
+
+        recall = true_positives / number_of_gt_bboxes
+        precision = true_positives / number_of_predicted_bboxes
+        miss_rate = 1 - recall
+
+        return precision, recall, miss_rate, results
 
