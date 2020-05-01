6fb396e8396997f3a51788626b650032ea18366d
Boychuk Vasily <pphator@gmail.com>
2020-05-01 21:39:41 +0300

    add utils and processing results modules to init

--- cvints/__init__.py
+++ cvints/__init__.py
@@ -2,4 +2,6 @@ from cvints.dataset import *
 from cvints.model import *
 from cvints.visialization import *
 from cvints.experiment import *
+from cvints.utils import *
+from cvints.processing_results import *
 
