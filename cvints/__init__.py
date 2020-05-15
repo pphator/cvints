4be2f7febde6b88669380701d768bfddf58c9ef6
Boychuk Vasily <pphator@gmail.com>
2020-05-15 15:55:02 +0300

    modify init file

--- cvints/__init__.py
+++ cvints/__init__.py
@@ -2,5 +2,7 @@ from cvints.dataset import *
 from cvints.model import *
 from cvints.visialization import *
 from cvints.experiment import *
-from cvints.utils import *
+from cvints.utils.utils import *
 from cvints.processing_results import *
+from cvints.utils import *
+from cvints.metrics import *
