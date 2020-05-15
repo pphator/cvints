e15af9446ee9bb7a712c9b01906940fc4a75aa71
Boychuk Vasily <pphator@gmail.com>
2020-05-15 15:56:36 +0300

    modify experiment to calculate metrics

--- cvints/experiment.py
+++ cvints/experiment.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import numpy as np
-from cvints.utils import MS_COCO_CATEGORIES_DICT, COCO_INSTANCE_CATEGORY_NAMES
-import cvints.utils as cvints_utils
+from cvints.utils.utils import COCO_INSTANCE_CATEGORY_NAMES
+import cvints.utils.utils as cvints_utils
 import cvints.visialization as cvints_vis
+from cvints.metrics import Metrics
 
 
 class Tasks:
@@ -17,11 +17,24 @@ class Experiment:
     """
         Description of intercommunication data and models with some results in the end
     """
-    def __init__(self, task, model, dataset, results=None):
+    def __init__(self, task, model, dataset, results=None, metrics=None):
         self.task = task
         self.model = model
         self.dataset = dataset
         self.results = results
+        self._metrics = metrics
+
+    @property
+    def metrics(self):
+        return self._metrics
+
+    @metrics.setter
+    def metrics(self, value):
+        if isinstance(value, Metrics):
+            self._metrics = value
+        elif isinstance(value, list):
+            self._metrics = Metrics(self.dataset, self.results)
+            self._metrics.set_metrics(*value)
 
     def describe(self):
         print('Number of images: {}'.format(len(self.dataset)))
@@ -60,21 +73,14 @@ class Experiment:
 
 
         """
-
-        # get annotations
-
         for each_image_info in self.dataset.annotations['images_info']:
             file_name = each_image_info['file_name']
             image_id = each_image_info['id']
-            this_image_prediction = self.results.get_results_by_image(image_id)
+            this_image_prediction = self.results.get_results_by_image_id(image_id)
             image_path = self.dataset.path_to_data + file_name
             image = cvints_vis.open_image(image_path)
             image = cvints_vis.put_bboxes_to_image(image, this_image_prediction)
-
             cvints_vis.show_image(image)
-
-
-
 
     def run(self):
         """
