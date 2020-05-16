9ec44bd0a0f3de57b999075488d0911a8dda5fcf
Boychuk Vasily <pphator@gmail.com>
2020-05-17 01:17:13 +0300

    remove opensets from autonomus package

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setuptools.setup(
      long_description_content_type="text/markdown",
      url="https://github.com/VasilyBoychuk/cvints",
      packages=setuptools.find_packages(),
-     package_data={'cvints.opensets': data_files},
+     # package_data={'opensets': data_files},
      # data_files=data_files,
      include_package_data=True,
      classifiers=[
